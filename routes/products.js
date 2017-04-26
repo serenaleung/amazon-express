0c9e04e9993f8d445684905c5f0920c3d5d1e5ca
Serena Leung <serenaleung1@gmail.com>
2017-04-26 11:31:12 -0700

    Linked products to Products#show page

--- routes/products.js
+++ routes/products.js
@@ -11,4 +11,14 @@ router.get('/', function (request, response, next){
     })
 })
 
+router.get('/:id', function (req, res) {
+  const id = req.params.id;
+
+  Product
+  .findById(id)
+  .then(function(product) {
+    res.render('products/show', { product: product })
+  });
+})
+
 module.exports = router;
