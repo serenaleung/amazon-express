325b36ed821427f39487987b0ff06d8b9857d962
Serena Leung <serenaleung1@gmail.com>
2017-04-27 10:31:09 -0700

    Added Products#new form, it's view, and Products#create. Also ordered products by most recent on top

--- routes/products.js
+++ routes/products.js
@@ -5,10 +5,16 @@ const Product = require('../models/index').Product;
 router.get('/', function (request, response, next){
   // response.send('Stuff');
   Product
-    .findAll()
+    .findAll({order: [['createdAt', 'DESC'], ['updatedAt', 'DESC']]})
     .then(function(products){
       response.render('products/index', { products:products });
     })
+})
+
+router.get('/new', function (req, res) {
+  const product = Product.build();
+
+  res.render('products/new', {product: product});
 })
 
 router.get('/:id', function (req, res) {
@@ -19,6 +25,19 @@ router.get('/:id', function (req, res) {
   .then(function(product) {
     res.render('products/show', { product: product })
   });
+})
+
+
+router.post('/', function (req, res) {
+  // res.send(req.body);
+
+  const {title, description, price} = req.body;
+
+  Product
+  .create({title: title, desciption: description, price: price})
+  .then(function (product) {
+    res.redirect('/products');
+  })
 })
 
 module.exports = router;
